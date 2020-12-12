1c8a15b738af4386598c936fb6b35b0661953ad5
hanzala <muhammadhanzala12@gmail.com>
2020-12-13 00:08:35 +0500

    Update cloudrun script

---
+++ cloudrun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+no_of_keys=$(cat env.json | jq 'keys[]' | wc -l)
+env_vars=""
+
+for i in $( seq 1 $no_of_keys )
+do
+env_vars+=$(cat env.json | jq keys[]  |  sed -n "$i p")=$(cat env.json | jq values[]  |  sed -n "$i p") 
+env_vars+=","
+done
+
+echo $env_vars
+gcloud run deploy ${FEATURE_ID} --image gcr.io/${PROJECT_ID}/${FEATURE_ID}:${COMMIT_SHA} --region us-central1 --platform managed --port 8080
+# for i in {0..$no_of_keys}
+# do
+#   echo "Number: $i"
+# done
