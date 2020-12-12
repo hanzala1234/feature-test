a769b326e096b6690bdd6a07e169b381a0149b39
hanzala <muhammadhanzala12@gmail.com>
2020-12-13 00:18:59 +0500

    Update script

--- cloudrun.sh
+++ cloudrun.sh
@@ -2,7 +2,7 @@
 
 no_of_keys=$(cat env.json | jq 'keys[]' | wc -l)
 env_vars=""
-
+apt install jq
 for i in $( seq 1 $no_of_keys )
 do
 env_vars+=$(cat env.json | jq keys[]  |  sed -n "$i p")=$(cat env.json | jq values[]  |  sed -n "$i p") 
@@ -10,7 +10,7 @@ env_vars+=","
 done
 
 echo $env_vars
-gcloud run deploy ${FEATURE_ID} --update-env-vars $env_vars --image gcr.io/${PROJECT_ID}/${FEATURE_ID}:${COMMIT_SHA}  --all-unauthenticated --region us-central1 --platform managed --port 8080
+gcloud run deploy ${FEATURE_ID} --update-env-vars $env_vars --image gcr.io/${PROJECT_ID}/${FEATURE_ID}:${COMMIT_SHA}  --allow-unauthenticated --region us-central1 --platform managed --port 8080
 # for i in {0..$no_of_keys}
 # do
 #   echo "Number: $i"
