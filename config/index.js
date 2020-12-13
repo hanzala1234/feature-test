3091e5d0d51e82cc41a7cbe893e2c0107ece09b1
hanzala <muhammadhanzala12@gmail.com>
2020-12-13 22:00:21 +0500

    Update script

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const bool = require('yn');
 
-const featureNames = require('./featuresRoutes.json');
+const featureNames = require('./features.json').filter((name) => name !== 'example');
 
 /**
  * @type {import('../app/typescript').FeatureConfig[]}
@@ -11,30 +11,32 @@ const featuresConfigs = featureNames.map(
   (featureName) => require(`../features/${featureName}/info.json`),
 );
 
-// DATABASES = 'feature_id%username:password@34.122.12.127/some-feature;...'
-// ? process.env.DATABASES.split(';').forEach((dbCredsStr) => {
-// ?   const parsed = dbCredsStr.match(/(.+)%(.+):(.+)@(.+)\/(.+)/);
+if (featureNames.length) {
+  // DATABASES = 'feature_id%username:password@34.122.12.127/some-feature;...'
+  // ? process.env.DATABASES.split(';').forEach((dbCredsStr) => {
+  // ?   const parsed = dbCredsStr.match(/(.+)%(.+):(.+)@(.+)\/(.+)/);
 
-// ?   featuresConfigs
-// ?     .find((featureConfig) => featureConfig.id === parsed[1])
-// ?     .dbCredentials = {
-// ?       user: parsed[2],
-// ?       password: parsed[3],
-// ?       server: parsed[4],
-// ?       dbName: parsed[5],
-// ?     };
-// ? });
+  // ?   featuresConfigs
+  // ?     .find((featureConfig) => featureConfig.id === parsed[1])
+  // ?     .dbCredentials = {
+  // ?       user: parsed[2],
+  // ?       password: parsed[3],
+  // ?       server: parsed[4],
+  // ?       dbName: parsed[5],
+  // ?     };
+  // ? });
 
-// CORE_API_CREDENTIALS = 'feature_id%username:password;...'
-process.env.CORE_API_CREDENTIALS.split(';').forEach((coreCredsStr) => {
-  const parsed = coreCredsStr.match(/(.+)%(.+):(.+)/);
+  // CORE_API_CREDENTIALS = 'feature_id%username:password;...'
+  process.env.CORE_API_CREDENTIALS.split(';').forEach((coreCredsStr) => {
+    const parsed = coreCredsStr.match(/(.+)%(.+):(.+)/);
 
-  featuresConfigs.find((featureConfig) => featureConfig.id === parsed[1])
-    .coreApiCredentials = {
-      user: parsed[2],
-      password: parsed[3],
-    };
-});
+    featuresConfigs.find((featureConfig) => featureConfig.id === parsed[1])
+      .coreApiCredentials = {
+        user: parsed[2],
+        password: parsed[3],
+      };
+  });
+}
 
 module.exports = {
   featuresConfigs,
